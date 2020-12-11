df23e3ddc3410faf108dfbe353d806cfaea8ca3b
Sabre <bazzamcbaz@gmail.com>
2020-12-12 09:26:18 +1100

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('My name is Earl!'));
+app.get('/', (req, res) => res.send('My name is Earl you lovely lovely man!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
